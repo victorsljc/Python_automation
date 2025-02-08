cf54841949c592a6bc1c3d2e4c92332d75ebaa2f
victorsljc <victorsljc@gmail.com>
2025-02-08 21:06:51 +0530

    selenium concepts

---
+++ Python_automation_devops/Git/test_git_doc.py
@@ -0,0 +1,31 @@
+'''Here are some git commands
+
+Getting Started
+        git init: Initialize a local Git repository
+        git clone [url]: Create a local copy of a remote repository
+        git config --global user.name "[name]": Set your username
+        git config --global user.email "[email]": Set your email
+
+Basic Operations
+        git status: Check the status of your repository12
+        git add [file]: Add a file to the staging area12
+        git add -A: Add all new and changed files1
+        git commit -m "[message]": Commit changes with a message
+
+Branching
+        git branch: List branches
+        git branch [name]: Create a new branch
+        git checkout [branch]: Switch to a branch
+        git merge [branch]: Merge branches
+
+Remote Repository
+        git push: Push changes to remote repository
+        git pull: Update local repository
+        git remote add origin [url]: Add a remote repository
+
+Additional Useful Commands
+        git log: View commit history
+        git diff: Show file differences
+        git stash: Temporarily store changes
+'''
+
